e42f2cea65e4f8f5a879e28bfdaaf1a4f2a9eac9
alvaro.gonzalez <alvaro.gonzalez@csc.fi>
2022-02-08 15:08:43 +0200

    Add remote-exec and reorganize variables into a separarte file

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-# Variables
-variable "keypair" {
-  type    = string
-  default = "XXXXX"   # name of keypair that will have access to the VM 
-}
-
-variable "network" {
-  type    = string
-  default = "XXXXX" # default network to be used
-}
-
-variable "security_groups" {
-  type    = list(string)
-  default = ["XXXX"]  # Name of default security group
-}
-
-# Data sources
-## Get Image ID
-data "openstack_images_image_v2" "image" {
-  name        = "CentOS-7" # Name of image to be used
-  most_recent = true
-}
-
 ## Get flavor id
 data "openstack_compute_flavor_v2" "flavor" {
   name = "standard.tiny" # flavor to be used
@@ -39,7 +16,28 @@ resource "openstack_compute_instance_v2" "server" {
   }
 }
 
-# Output VM IP Address
-output "serverip" {
- value = openstack_compute_instance_v2.server.access_ip_v4
+# Add Floating ip
+
+resource "openstack_networking_floatingip_v2" "fip1" {
+  pool = "public"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip1" {
+  floating_ip = openstack_networking_floatingip_v2.fip1.address
+  instance_id = openstack_compute_instance_v2.server.id
+
+  provisioner "remote-exec" {
+    inline = ["echo 'Hello World'"]
+
+    connection {
+      type        = "ssh"
+      user        = "${var.ssh_user}"
+      host        = "${openstack_networking_floatingip_v2.fip1.address}"
+      private_key = "${file("${var.private_key_path}")}"
+    }
+  }
+}
+
+output "server_floating_ip" {
+ value = openstack_networking_floatingip_v2.fip1.address
 }
